804a7f01cd91f6e1658636f4e2460bf112974cf3
TheRealCasiv2 <60825241+TheRealCasiv2@users.noreply.github.com>
2022-01-22 18:10:51 +0100

    added files

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+    'data/*.meta'
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
